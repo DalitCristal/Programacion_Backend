c9bcf1d653dbdf5f7d3a456204a03c8dda31d32a
venite_a_punilla <dalacristal25@gmail.com>
2023-08-12 23:36:10 -0300

    tercer desafio

--- App.js
+++ App.js
@@ -5,21 +5,13 @@ const PORT = 4000;
 
 const app = express();
 const ProductosManager = new ProductManager("./products.json");
-//const productos = await ProductosManager.getProducts();
+const productos = await ProductosManager.getProducts();
 
 app.get("/", (req, res) => {
   res.send("Hola");
 });
 
-app.get("/products", async (req, res) => {
-  const { limit } = req.query;
-  const products = await ProductosManager.getProducts();
-  const prods = products.slice(0, limit);
-});
-
-/* 
 app.get("/products", (req, res) => {
-  console.log(req.query);
   const { title } = req.query;
 
   if (title) {
@@ -38,7 +30,7 @@ app.get("/products/:id", (req, res) => {
   } else {
     res.send("Producto no encontrado");
   }
-}); */
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor en el puerto ${PORT} `);
